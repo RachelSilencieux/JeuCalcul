9845f1d7a89dff5bce4ed1ddb75b635d4633bcc5
RachelSilencieux <2139769@collegemv.qc.ca>
2024-04-25 13:10:34 -0400

    efkgqkwfhwf

--- jeu.js
+++ jeu.js
@@ -1,19 +1,27 @@
-import * as PIXI from 'pixi.js';
-
-const jeu = new PIXI.Application({width: 800, height: 600});
-
-document.body.appendChild(jeu.view);
-
-PIXI.Loader.shared.add("background", "ProjetLabyrinthe\img\grass.jpg").load(setup);
-
-function setup(){
-
-   let background = new PIXI.Sprite(PIXI.Loader.shared.ressources["background"].texture);
-
-   background.width = jeu.screen.width;
-   background.height = jeu.screen.height;
-
-   jeu.stage.addChild(background);
-}
-
-setup();
+//const { Graphics } = require("pixi.js");
+ 
+var x = 0;
+var y = 0;
+var width = 30;
+var height = 30;
+ 
+ 
+const app  = new PIXI.Application({width: 800, height: 600, background: '#1099bb'});
+ 
+// app.init({ width: 800, height: 600, background: '#1099bb'});
+ 
+ 
+document.getElementById('pixi-container').appendChild(app.view)
+ 
+const texturePromise = PIXI.Assets.load('/img/grass.jpg');
+ 
+const graphics = new Graphics();
+     
+graphics.rect(50, 50, 100, 100);
+graphics.fill(0xf0000);
+ 
+app.stage.addChild(graphics);  
+ 
+ 
+ 
+// new PIXI.Graphics(x, y, width, height);
